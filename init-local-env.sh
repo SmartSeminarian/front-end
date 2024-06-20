a5f7627b42f86ac23a69237646b5dffc6b934843
Michalis Christodoulou <michalis.christodoulou@jetbrains.com>
2024-06-20 18:06:35 +0300

    local deployment

---
+++ init-local-env.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+BRANCH=`git rev-parse --abbrev-ref HEAD`
+
+REPO=`git rev-parse --show-toplevel`
+
+remote_url=$(git remote get-url origin)
+
+# Check if the remote URL was fetched successfully
+if [ -z "$remote_url" ]; then
+  echo "Failed to retrieve git URL"
+  exit 1
+fi
+
+# Extract the organization or account name based on the URL format
+if [[ $remote_url == https://github.com/* ]]; then
+  ORG_NAME=$(echo $remote_url | sed -E 's|https://github.com/([^/]+)/.*|\1|')
+elif [[ $remote_url == git@github.com:* ]]; then
+  ORG_NAME=$(echo $remote_url | sed -E 's|git@github.com:([^/]+)/.*|\1|')
+else
+  echo "URL format not recognized."
+  exit 1
+fi
+
+ORG_NAME=$(echo "$ORG_NAME" | tr '[:upper:]' '[:lower:]')
+
+SERVICE_NAME=`basename $REPO`
+CONTAINER_NAME=$SERVICE_NAME
+
+T=`date +%Y-%m-%d-%H-%M-%S`
+mv ./deploy/.env ./deploy/.env.$T
+
+VERSION_FILE=./VERSION
+
+echo "PROJECT=$ORG_NAME" > ./.env
+echo "REPO=$REPO" >> ./.env
+echo "BRANCH=$BRANCH" >> ./.env
+echo "SERVICE_NAME=$SERVICE_NAME" >> ./.env
+echo "CONTAINER_NAME=$CONTAINER_NAME"  >> ./.env
+echo "CONTAINER_DATA=./.data/"  >> ./.env
+echo "VERSION_FILE=$VERSION_FILE"  >> ./.env
+echo "Version: $BRANCH `hostname` `date`" > $VERSION_FILE
+
+cat ./.env
+mkdir -p ./.data/
+cp ./deploy/docker-compose.yml ./docker-compose.yml
+sed "s/^#UNCOMMENT_ME_FOR_DEV//" ./deploy/docker-compose.yml > ./docker-compose.yml
