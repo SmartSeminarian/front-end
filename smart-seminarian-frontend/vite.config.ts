066d6c3e152f61ea927986b6a1629c6f1b46bbe8
Michalis Christodoulou <michalis.christodoulou@jetbrains.com>
2024-07-22 21:38:03 +0300

    fix config issue

--- smart-seminarian-frontend/vite.config.ts
+++ smart-seminarian-frontend/vite.config.ts
@@ -1,12 +1,18 @@
+/// <reference types="vitest" />
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { svelteTesting } from '@testing-library/svelte/vite'
 import path from 'path';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), svelteTesting()],
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, 'src')
 		}
-	}
+	},
+	test: {
+        globals: true,
+        environment: 'jsdom',
+    },
 });
